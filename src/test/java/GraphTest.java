7381e039ad9181eb53ec3df100cfc3b1d93ab8de
Maya Tene <maya@Piglets-Tail.local>
2021-11-30 09:38:00 -0800

    Adds unit test methods for Inteface Classes

--- src/test/java/GraphTest.java
+++ src/test/java/GraphTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.beans.Transient;
+import java.lang.invoke.StringConcatException;
 import java.lang.reflect.InvocationTargetException;
 
 public class GraphTest 
@@ -171,37 +172,7 @@ public class GraphTest
         assertTrue(Arrays.equals(traversalOrder.toArray(), expectedOrder));
     }
 
-    private static <T> void printBreadthFirst(GraphInterface<T> graph, T originLabel)
-    {
-        T[] traversalArray = (T[]) graph.breadthFirstTraverse(originLabel).toArray();
-        System.out.println("Breadth-First Traversal: " + traversalArray.toString());
-    }
-
-    private static <T> void printDepthFirst(GraphInterface<T> graph, T originLabel)
-    {
-        T[] traversalArray = (T[]) graph.depthFirstTraverse(originLabel).toArray();
-        System.out.println("Depth-First Traversal: " + traversalArray.toString());
-    }
-    
-    public static void main(String[] args)
-    {
-        String[] vertexLabels = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
-        //graph description from project assignment:
-        String[][] edges = {{"A", "B"}, {"A", "D"}, {"A", "E"}, {"B", "E"}, 
-                        {"D", "G"}, {"E", "F"}, {"E", "F"}, {"E", "H"}, {"G", "H"}, 
-                        {"F", "C"}, {"F", "H"}, {"H", "I"}, {"C", "B"}, {"I", "F"}};
-
-        AdjacencyMatrixGraph <String> matrixGraph = new AdjacencyMatrixGraph<>(vertexLabels, edges);
-        AdjacencyListGraph <String> listGraph = new AdjacencyListGraph<>(vertexLabels, edges);
-        System.out.println("Traversal under Adjacency Matrix Graph Implementation:");
-        printBreadthFirst(matrixGraph, "A");
-        printDepthFirst(matrixGraph, "A");
-
-        System.out.println("Traversal under Adjacency List Graph Implementation:");
-        printBreadthFirst(listGraph, "A");
-        printDepthFirst(listGraph, "A");
-    }
-
+    // Utility constructors to allow tests to be parametrized:
     static GraphInterface constructGraph(Class c, int a)
     {
         if (c.equals(AdjacencyListGraph.class)) {
@@ -233,6 +204,110 @@ public class GraphTest
             return new AdjacencyMatrixGraph<>(a, b);
         }
         throw new IllegalArgumentException("unexpected class type");
+    }
+
+
+    static String[] vertexLabels = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+    //graph description from project assignment:
+    static String[][] edges = {{"A", "B"}, {"A", "D"}, {"A", "E"}, {"B", "E"}, 
+                    {"D", "G"}, {"E", "F"}, {"E", "F"}, {"E", "H"}, {"G", "H"}, 
+                    {"F", "C"}, {"F", "H"}, {"H", "I"}, {"C", "B"}, {"I", "F"}};
+
+    static String expectedBreadthFirstOutput = "A B D E G F H C I ";
+    static String expectedDepthFirstOutput = "A B E F C H I D G ";
+
+    @Test
+    void project5MatrixTestDepthFirst() 
+    {
+        String output = project5MatrixDepthFirst();
+        assertEquals(output, expectedDepthFirstOutput);
+    }
+
+    String project5MatrixDepthFirst() 
+    {
+        AdjacencyMatrixGraph <String> matrixGraph = new AdjacencyMatrixGraph<>(vertexLabels, edges);
+        Queue<String> traversalOrder = matrixGraph.depthFirstTraverse("A");
+        StringBuilder output = new StringBuilder();
+        for (String s : traversalOrder)
+        {
+            output.append(s);
+            output.append(" ");
+        }
+        return output.toString();
+    }
+
+    @Test
+    void project5MatrixTestBreadthFirst()
+    {
+        String output = project5MatrixBreadthFirst();
+        assertEquals(output, expectedBreadthFirstOutput);
+    }
+
+    String project5MatrixBreadthFirst() 
+    {
+        AdjacencyMatrixGraph <String> matrixGraph = new AdjacencyMatrixGraph<>(vertexLabels, edges);
+        Queue<String> traversalOrder = matrixGraph.breadthFirstTraverse("A");
+        StringBuilder output = new StringBuilder();
+        for (String s : traversalOrder)
+        {
+            output.append(s);
+            output.append(" ");
+        }
+        return output.toString();
+    }
+
+    @Test
+    void project5ListTestDepthFirst()
+    {
+        String output = project5ListDepthFirst();
+        assertEquals(output, expectedDepthFirstOutput);
+    }
+
+    String project5ListDepthFirst() 
+    {
+        AdjacencyListGraph <String> matrixGraph = new AdjacencyListGraph<>(vertexLabels, edges);
+        Queue<String> traversalOrder = matrixGraph.depthFirstTraverse("A");
+        StringBuilder output = new StringBuilder();
+        for (String s : traversalOrder)
+        {
+            output.append(s);
+            output.append(" ");
+        }
+        return output.toString();
+    }
+
+    @Test
+    void project5ListTestBreadthFirst() 
+    {
+        String output = project5ListBreadthFirst();
+        assertEquals(output, expectedBreadthFirstOutput);
+    }
+
+    String project5ListBreadthFirst() 
+    {
+        AdjacencyListGraph <String> matrixGraph = new AdjacencyListGraph<>(vertexLabels, edges);
+        Queue<String> traversalOrder = matrixGraph.breadthFirstTraverse("A");
+        StringBuilder output = new StringBuilder();
+        for (String s : traversalOrder)
+        {
+            output.append(s);
+            output.append(" ");
+        }
+        return output.toString();
+    }
+    
+    public static void main(String[] args)
+    {
+        GraphTest test = new GraphTest();
+
+        System.out.println("Traversal under Adjacency Matrix Graph Implementation:");
+        System.out.println("Breadth-First Traversal: " + test.project5MatrixBreadthFirst());
+        System.out.println("Depth-First Traversal  : " + test.project5MatrixDepthFirst());
+
+        System.out.println("\nTraversal under Adjacency Matrix Graph Implementation:");
+        System.out.println("Breadth-First Traversal: " + test.project5ListBreadthFirst());
+        System.out.println("Depth-First Traversal  : " + test.project5ListDepthFirst());
+    
     }
 
 }
